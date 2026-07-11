c389b4920129b0167348cd6ad6fbb87e5a7f7ddc
xorh02 <xavierong02@gmail.com>
2026-07-11 06:26:34 +0000

    Jul 11, 2026, 6:26 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,8 @@ function setup() {
   // write your codes here
   ball = new Sprite();
   // End Basic shape testing
-   //circle(x,y,diameter)
+
+   //how to create a circle circle(x,y,diameter);
    // Create a bouncing ball sprite
    // write your codes here
    
