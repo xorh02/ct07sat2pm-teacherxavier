5b24c4541e990bd7667de72af207c9230081c04e
xorh02 <xavierong02@gmail.com>
2026-07-11 06:25:55 +0000

    Jul 11, 2026, 6:25 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,16 +1,16 @@
 function setup() {
   // Set up the canvas
-  new Canvas(800, 400);
+  new Canvas(400, 600);
   background(250); //background color
 
   // Basic shape testing
   // write your codes here
   ball = new Sprite();
   // End Basic shape testing
-
+   
    // Create a bouncing ball sprite
    // write your codes here
-
+   
 }
 
 function draw() {
