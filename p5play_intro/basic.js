d9251c0ed5af049e3d547530f87df991b16bd532
xorh02 <xavierong02@gmail.com>
2026-07-18 07:00:20 +0000

    Jul 18, 2026, 7:00 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -34,8 +34,9 @@ function setup() {
   // --- Exercise: Age category classification ---
   // write your codes here
   // if(condition){
-  // 
+  //   action
   // }else if(condition){
+  //    action
   // }
 
 
