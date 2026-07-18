47e335e3838ce424f608e475969ad3422898f9eb
xorh02 <xavierong02@gmail.com>
2026-07-18 06:41:09 +0000

    Jul 18, 2026, 6:41 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -12,7 +12,9 @@ function setup() {
 
   // --- Exercise: Area of Triangle ---
   // write your codes here
-  
+  area = base *height;
+  console.log(area);
+
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
