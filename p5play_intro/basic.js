cf330ac3d5af00e443f6a94f8e31e361bed3ff99
xorh02 <xavierong02@gmail.com>
2026-07-18 06:45:03 +0000

    Jul 18, 2026, 6:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,6 +16,8 @@ function setup() {
   console.log("The area of the triangle is :",area);
   //text(what u wan to print , x position of where u wan it to be, y position of where u wan it to be)
   text("The area of the triangle is:" + area,300,200);
+
+  
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
 
