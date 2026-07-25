0b24ea18f9eefd225e38799a0e4712afe321e4d4
xorh02 <xavierong02@gmail.com>
2026-07-25 07:33:03 +0000

    Jul 25, 2026, 7:33 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -1,1 +1,38 @@
 //final codes for 25/7/2026
+//declare variables
+let bird, floor;
+let midflapimg, bg ,base;
+
+//load assets in (Images & sound)
+function preload(){
+    //add in the image link 
+    midflapimg = loadImage('assets/redbird-midflap.png');
+    bg = loadImage('assets/background-night.png');
+    base = loadImage('assets/base.png');
+}
+
+function setup(){
+    createCanvas(400,600);
+    // background(255);//0-> black 255->white
+    //variable = new Sprite();
+    bird = new Sprite();
+    //initialise the properties w,h,x,y
+    bird.x = 200;
+    bird.y = 300;
+    bird.width = 20;
+    bird.height = 20;
+    bird.img = midflapimg;
+    //physics
+    bird.collider = 'dynamic';
+    bird.mass =2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.05;
+    //gravity
+    world.gravity.y = 10;
+
+}
+
+function draw(){
+    //adding the image to the background
+    image(bg,0,0,width , height);
+}
