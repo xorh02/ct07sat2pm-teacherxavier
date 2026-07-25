c41156904bb76035b111d0e62d55940cdd02dc06
xorh02 <xavierong02@gmail.com>
2026-07-25 07:11:08 +0000

    Jul 25, 2026, 7:11 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -5,7 +5,7 @@ let midflapimg, bg ,base;
 //load assets in (Images & sound)
 function preload(){
     //add in the image link 
-    midflapimg = loadImage('redbird-midflap.png');
+    midflapimg = loadImage('assets/redbird-midflap.png');
     bg = loadImage('background-night.png');
     base = loadImage('base.png');
 }
