b6087e9e506263aa6cb159dcf1f64d16f3e509f7
xorh02 <xavierong02@gmail.com>
2026-07-25 07:28:33 +0000

    Jul 25, 2026, 7:28 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -21,7 +21,7 @@ function setup(){
     bird.width = 20;
     bird.height = 20;
     bird.img = midflapimg;
-    bird.collider = 'static';
+    bird.collider = 'dynamic';
     bird.mass =2;
     bird.drag = 0.02;
     bird.bounciness = 0.05;
